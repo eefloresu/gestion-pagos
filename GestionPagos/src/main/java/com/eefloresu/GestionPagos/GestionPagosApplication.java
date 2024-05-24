093ef1b641bc6c0ad34f99aea5907032e518bd4e
eefloresu <ulloaelian215@gmail.com>
2024-05-24 16:56:10 -0600

    Primer commit del proyecto

---
+++ GestionPagos/src/main/java/com/eefloresu/GestionPagos/GestionPagosApplication.java
@@ -0,0 +1,13 @@
+package com.eefloresu.GestionPagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPagosApplication.class, args);
+	}
+
+}
---
+++ GestionPagos/src/test/java/com/eefloresu/GestionPagos/GestionPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eefloresu.GestionPagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
